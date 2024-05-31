24e34706fea61bfd0153b1f335b97c6dfe8d0e5d
SashaMaltsev <144154407+SashaMaltsev@users.noreply.github.com>
2024-05-31 19:52:39 +0300

    Update queue.hpp

---
+++ tasks/abstract/queue/queue.hpp
@@ -0,0 +1,45 @@
+#include "Deque/deque.hpp"
+
+
+template<typename T, typename Container = Deque<T>>
+class Queue : protected Container {
+public:
+    Queue() = default;
+
+    void Pop();
+
+    void Push(const T& value);
+    void Push(T&& value);
+
+    template <class... Args>
+    void Emplace(Args&& ... args);
+
+    using Container::IsEmpty;
+    using Container::Size;
+    using Container::Swap;
+    using Container::Front;
+
+    ~Queue() = default;
+};
+
+
+template <typename T, typename Container>
+inline void Queue<T, Container>::Push(const T& value) {
+    Emplace(value);
+}
+
+template <typename T, typename Container>
+inline void Queue<T, Container>::Push(T&& value) {
+    Emplace(std::move(value));
+}
+
+template <typename T, typename Container>
+template <class... Args>
+inline void Queue<T, Container>::Emplace(Args&& ...args) {
+    Container::EmplaceBack(args...);
+}
+
+template <typename T, typename Container>
+inline void Queue<T, Container>::Pop() {
+    Container::PopFront();
+}
