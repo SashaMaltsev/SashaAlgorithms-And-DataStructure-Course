76af81d6204405be02e4c82b909143538d5b33d0
SashaMaltsev <144154407+SashaMaltsev@users.noreply.github.com>
2024-06-05 17:11:01 +0300

    Update array.hpp

--- tasks/abstract/deque/files/array.hpp
+++ tasks/abstract/deque/files/array.hpp
@@ -3,7 +3,6 @@
 #include "vector.hpp"
 #include "allocator.hpp"
 
-
 template<typename T, typename Allocator = Allocator<T>>
 class Array : protected Vector<T, Allocator> {
 
@@ -55,6 +54,11 @@ Array<T, Allocator>::Array() : start_(0), end_(0) {
 
 template<typename T, typename Allocator>
 Array<T, Allocator>::Array(const Array& other) : Vector<T, Allocator>(other), start_(other.start_), end_(other.end_) {
+    if (this->end_ < this->start_) {
+        for (size_t i = start_; i < this->Vector<T, Allocator>::capacity_; ++i) {
+            this->Vector<T, Allocator>::allocator_.construct(this->data_ + i, *(other.data_ + i));
+        }
+    }
 }
 
 template<typename T, typename Allocator>
